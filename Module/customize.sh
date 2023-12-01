a0f14571903ac18140976296ebdd526776c0a240
reigadegr <2722688642@qq.com>
2023-12-01 16:21:36 +0800

    更新magisk模块

--- Module/customize.sh
+++ Module/customize.sh
@@ -1,3 +1,8 @@
 SKIPUNZIP=0
 MODDIR=${0%/*}
-echo "配置文件在/data/adb/modules/scene_systemless/irisHelper_cpp/config/iriscfgcustomize.conf"
+echo "配置文件在/data/adb/modules/irisHelper_cpp/config/iriscfgcustomize.conf"
+echo "与Hydro_BrÛleur配置文件通用，可以把其配置文件覆盖到本模块的对应位置"
+echo "必须对配置文件内的app进行至少一项的参数配置"
+if [ -f /data/adb/modules/irisHelper_cpp/config/iriscfgcustomize.conf ]; then
+    cp -af /data/adb/modules/irisHelper_cpp/config/iriscfgcustomize.conf /data/adb/modules_update/irisHelper_cpp/config/iriscfgcustomize.conf
+fi
