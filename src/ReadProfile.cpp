714e6fe17e8d603f1515820b2c4bf48b0cd16faf
reigadegr <2722688642@qq.com>
2023-12-02 15:32:56 +0800

    删除读取文件的while循环

--- src/ReadProfile.cpp
+++ src/ReadProfile.cpp
@@ -57,109 +57,96 @@ auto readProfile(const char *profile, std::vector<irisConfig> &conf) -> bool
 	conf.clear();
 	std::string buf;
 	LOG("加载配置文件...\n");
-	while (true) {
-		std::string app = "";
-		std::string params_a = "";
-		std::string params_b = "";
-		std::string params_c = "";
-		std::string params_d = "";
-		std::string df = "";
-		std::string perfmgr_enable = "0";
-		std::string fixed_target_fps = "-1";
-		std::string perfmgr_powersave = "N";
-		bool flag = false;
-		while (std::getline(file, buf)) {
-			if (buf[0] == '#' || buf.empty()) {
-				continue;
-			}
 
-			if (buf.find("app") != std::string::npos) {
-				if ((app != "") && (flag = true)) {
-					if (!(params_a == "" &&
-					      params_b == "" &&
-					      params_c == "" &&
-					      params_d == "" && df == "" &&
-					      perfmgr_enable == "0" &&
-					      fixed_target_fps == "-1" &&
-					      perfmgr_powersave == "N")) {
-						std::lock_guard<std::mutex> lock(
-							confMutex);
-						conf.push_back(
-							{ app.c_str(),
-							  params_a.c_str(),
-							  params_b.c_str(),
-							  params_c.c_str(),
-							  params_d.c_str(),
-							  df.c_str(),
-							  atoi(perfmgr_enable
-								       .c_str()),
-							  atoi(fixed_target_fps
-								       .c_str()),
-							  perfmgr_powersave
-								  .c_str() });
-					}
-					// reset value
-					params_a = "";
-					params_b = "";
-					params_c = "";
-					params_d = "";
-					df = "";
-					perfmgr_enable = "0";
-					fixed_target_fps = "-1";
-					perfmgr_powersave = "N";
-				}
-				GetSecondArg(buf);
-				reduceStr(buf);
-				app = buf;
-				flag = true;
-				continue;
-			}
+	std::string app = "";
+	std::string params_a = "";
+	std::string params_b = "";
+	std::string params_c = "";
+	std::string params_d = "";
+	std::string df = "";
+	std::string perfmgr_enable = "0";
+	std::string fixed_target_fps = "-1";
+	std::string perfmgr_powersave = "N";
+	bool flag = false;
+	while (std::getline(file, buf)) {
+		if (buf[0] == '#' || buf.empty()) {
+			continue;
+		}
 
-			if (strCtrl(buf, "params_a", params_a)) {
-				continue;
-			}
-			if (strCtrl(buf, "params_b", params_b)) {
-				continue;
-			}
-			if (strCtrl(buf, "params_c", params_c)) {
-				continue;
-			}
-			if (strCtrl(buf, "params_d", params_d)) {
-				continue;
-			}
-			if (strCtrl(buf, "df", df)) {
-				continue;
-			}
-			if (strCtrl(buf, "perfmgr_enable", perfmgr_enable)) {
-				if (perfmgr_enable != "0" &&
-				    perfmgr_enable != "1") {
-					perfmgr_enable = "0";
+		if (buf.find("app") != std::string::npos) {
+			if ((app != "") && (flag = true)) {
+				if (!(params_a == "" && params_b == "" &&
+				      params_c == "" && params_d == "" &&
+				      df == "" && perfmgr_enable == "0" &&
+				      fixed_target_fps == "-1" &&
+				      perfmgr_powersave == "N")) {
+					std::lock_guard<std::mutex> lock(
+						confMutex);
+					conf.push_back(
+						{ app.c_str(), params_a.c_str(),
+						  params_b.c_str(),
+						  params_c.c_str(),
+						  params_d.c_str(), df.c_str(),
+						  atoi(perfmgr_enable.c_str()),
+						  atoi(fixed_target_fps.c_str()),
+						  perfmgr_powersave.c_str() });
 				}
-				continue;
+				// reset value
+				params_a = "";
+				params_b = "";
+				params_c = "";
+				params_d = "";
+				df = "";
+				perfmgr_enable = "0";
+				fixed_target_fps = "-1";
+				perfmgr_powersave = "N";
 			}
-			if (strCtrl(buf, "fixed_target_fps",
-				    fixed_target_fps)) {
-				continue;
+			GetSecondArg(buf);
+			reduceStr(buf);
+			app = buf;
+			flag = true;
+			continue;
+		}
+
+		if (strCtrl(buf, "params_a", params_a)) {
+			continue;
+		}
+		if (strCtrl(buf, "params_b", params_b)) {
+			continue;
+		}
+		if (strCtrl(buf, "params_c", params_c)) {
+			continue;
+		}
+		if (strCtrl(buf, "params_d", params_d)) {
+			continue;
+		}
+		if (strCtrl(buf, "df", df)) {
+			continue;
+		}
+		if (strCtrl(buf, "perfmgr_enable", perfmgr_enable)) {
+			if (perfmgr_enable != "0" && perfmgr_enable != "1") {
+				perfmgr_enable = "0";
 			}
-			if (strCtrl(buf, "perfmgr_powersave",
-				    perfmgr_powersave)) {
-				continue;
-			}
+			continue;
 		}
-		if (app == "") {
-			break;
+		if (strCtrl(buf, "fixed_target_fps", fixed_target_fps)) {
+			continue;
 		}
-		if (!(params_a == "" && params_b == "" && params_c == "" &&
-		      params_d == "" && df == "" && perfmgr_enable == "0" &&
-		      fixed_target_fps == "-1" && perfmgr_powersave == "N")) {
-			std::lock_guard<std::mutex> lock(confMutex);
-			conf.push_back({ app.c_str(), params_a.c_str(),
-					 params_b.c_str(), params_c.c_str(),
-					 params_d.c_str(), df.c_str(),
-					 atoi(perfmgr_enable.c_str()),
-					 atoi(fixed_target_fps.c_str()),
-					 perfmgr_powersave.c_str() });
+		if (strCtrl(buf, "perfmgr_powersave", perfmgr_powersave)) {
+			continue;
 		}
+	}
+
+	if (!(params_a == "" && params_b == "" && params_c == "" &&
+	      params_d == "" && df == "" && perfmgr_enable == "0" &&
+	      fixed_target_fps == "-1" && perfmgr_powersave == "N")) {
+		std::lock_guard<std::mutex> lock(confMutex);
+		conf.push_back({ app.c_str(), params_a.c_str(),
+				 params_b.c_str(), params_c.c_str(),
+				 params_d.c_str(), df.c_str(),
+				 atoi(perfmgr_enable.c_str()),
+				 atoi(fixed_target_fps.c_str()),
+				 perfmgr_powersave.c_str() });
 	}
 	file.close();
 	print_struct(conf);
