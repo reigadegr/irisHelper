464d2f771e052f6bfcc74cb256f5d04487c06d93
reigadegr <2722688642@qq.com>
2023-12-03 13:21:53 +0800

    优化代码结构

--- src/FindPerfmgr.cpp
+++ src/FindPerfmgr.cpp
@@ -1,5 +1,7 @@
 #include <filesystem>
-// #include "include/LOG.h"
+
+#include "include/LOG.h"
+#include "include/Path.h"
 namespace fs = std::filesystem;
 //  参数分别为:
 //  目录名，目标文件名，裁剪的字符串
@@ -24,4 +26,33 @@ auto FindPerfmgrName(const std::string &dir, const std::string &str,
         }
     }
     return "UnSupport";
+}
+static auto whetherPerfmgrExists() -> std::string
+{
+    // 先检测perfmgr_mtk节点
+    if (!std::filesystem::exists("/sys/module/perfmgr_mtk")) {
+        LOG("没刷YuniKernel");
+        static std::string perfmgrName =
+            FindPerfmgrName("/sys/module", "perfmgr_enable", "/parameters");
+        if (perfmgrName == "UnSupport") {
+            LOG("feas尊嘟不支持你的设备");
+            return "UnSupport";
+        }
+        return perfmgrName;
+    }
+    return "perfmgr_mtk";
+}
+void initFeasPath(struct FeasPath *p)
+{
+    std::string perfmgrName = whetherPerfmgrExists();
+    if (perfmgrName == "UnSupport") {
+        p->enable = "";
+        p->fps = "";
+        return;
+    }
+    p->enable = ("/sys/module/" + perfmgrName + "/parameters/perfmgr_enable");
+    p->fps =
+        ("/sys/module/" + perfmgrName + "/parameters/fixed_target_fps").c_str();
+    p->powersave =
+        ("/sys/module/" + perfmgrName + "/parameters/powersave").c_str();
 }
