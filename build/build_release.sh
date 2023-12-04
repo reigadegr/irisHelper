febbb2b4db23b61f711a48c916b28b7a1f3d68ce
reigadegr <2722688642@qq.com>
2023-12-04 12:00:01 +0800

    修改编译脚本

--- build/build_release.sh
+++ build/build_release.sh
@@ -38,8 +38,9 @@ compile_start() {
 	chmod +x $for_test
 	cd $(dirname "$0")/../Module
 	version=$(grep "version=" $(pwd)/module.prop | cut -d "=" -f2)
+	commit=$(grep "main" $(dirname "$0")/../.git/packed-refs | awk 'NR==1{print $1}' | cut -c 1-5)
 	rm $(pwd)/../output/irisHelper*.zip $(pwd)/*.bak $(pwd)/*/*.bak $(pwd)/*.log 2>/dev/null
-    /data/data/com.termux/files/usr/bin/zip -9 -rq "$(pwd)/../output/irisHelperV$version.zip" .
+    /data/data/com.termux/files/usr/bin/zip -9 -rq "$(pwd)/../output/irisHelperV$version $commit.zip" .
 	echo "当前时间：$(date +%Y) 年 $(date +%m) 月 $(date +%d) 日 $(date +%H) 时 $(date +%M) 分 $(date +%S) 秒"
 }
 
@@ -47,4 +48,5 @@ format_code
 compile_start
 remove_file 2>/dev/null
 ldd $project_root/output/irisHelper 2>/dev/null
+
 exit 0
--- build_speed/build_speed.sh
+++ build_speed/build_speed.sh
@@ -21,6 +21,8 @@ cppFile_ctrl(){
 }
 format_code() {
 	echo "当前时间：$(date +%Y) 年 $(date +%m) 月 $(date +%d) 日 $(date +%H) 时 $(date +%M) 分 $(date +%S) 秒"
+	/data/data/com.termux/files/usr/bin/clang-format -i $project_root/src/*.cpp
+    /data/data/com.termux/files/usr/bin/clang-format -i $project_root/src/include/*.h
 	/data/data/com.termux/files/usr/bin/clang-format -i $(pwd)/*.cpp
     echo "格式化代码完毕"
 }
@@ -50,11 +52,12 @@ compile_start() {
 	chmod +x $for_test
 	cd $(dirname "$0")/../Module
 	version=$(grep "version=" $(pwd)/module.prop | cut -d "=" -f2)
+	commit=$(grep "main" $(dirname "$0")/../.git/packed-refs | awk 'NR==1{print $1}' | cut -c 1-5)
 	rm $(pwd)/../output/irisHelper*.zip $(pwd)/*.bak $(pwd)/*/*.bak $(pwd)/*.log 2>/dev/null
-    /data/data/com.termux/files/usr/bin/zip -9 -rq "$(pwd)/../output/irisHelperV$version.zip" .
+    /data/data/com.termux/files/usr/bin/zip -9 -rq "$(pwd)/../output/irisHelperV$version $commit.zip" .
 	echo "当前时间：$(date +%Y) 年 $(date +%m) 月 $(date +%d) 日 $(date +%H) 时 $(date +%M) 分 $(date +%S) 秒"
 }
-
+echo "本脚本仅供测试是否编译通过"
 cppFile_ctrl
 format_code
 compile_start
